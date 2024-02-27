8afaeb824cce0c6319c26d8e1ec5501406e78da4
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-02-28 00:24:23 +0100

    Add tensordict state computation

--- game/rl_environment/game_env.py
+++ game/rl_environment/game_env.py
@@ -1,40 +1,4 @@
 """TorchRL environment wrapper for the game environment.
-
-TODO: voir comment transformer le format de l'environnement en tensordict
-+ pour le DQN qui n'a pas besoin de ça à priori ?
-
-TODO: définir notre manière de convertir to & from environments.
-on ne va pas forcément embed tout de la même manière ?
-
-=> ennemis : créer une "liste de tenseurs" (ajouter une dimension au bon endroit, le dernier) pour que ça puisse
-être processed facilement
-=> mettre des utility functions dans un truc à côté.
-[batch size, max enemies, enemy features]
-
-padding strategy:
-* use zeros
-* maybe add a 5th value that is 1 if enemy is present, 0 else (to avoid confusion with padding)
-
-=> normalize input values around 0 so that padding with 0 makes sense.
-
-normalization: use the game settings ! normalize speed based on the max speed available (player x bullet x enemy)
-same for space, use the game map for position normalization.
-
-mask tensor ? ça va dépendre de ce dont on a besoin pour les calculs qu'on fait sur les ennemis.
-
----
-calculs ennemis
-
--> si on les process 1 par 1 pour les sommer : à la fin, on peut multiplier par le masque
--> si on fait un "fixed-size" pour calculer ça: un masque c'est pas idéal ? Il faudrait plutôt un indicateur de padding,
-sinon il va croire qu'il y a plein d'ennemis immobiles en 0.
-
-=> à voir si c'est à l'env wrapper d'émettre ce masque ? Via un tensordict c'est pas gênant.
-
----
-séparer en fichiers:
-=> des helpers pour convertir tensor <-> env et back
-=> composition : différentes manières de représenter ça dans l'env, utiliser des sous classes composables ? (à la torch)
 """
 
 import torch
@@ -42,6 +6,9 @@ from tensordict import TensorDict, TensorDictBase
 from torchrl.data.utils import DEVICE_TYPING
 from torchrl.envs import EnvBase
 
+from game.backend.entities.base_entity import EntityType
+from game.backend.entities.bullet_entity import BulletEntity
+from game.backend.entities.enemy_entity import EnemyEntity
 from game.backend.environment import Environment
 from game.backend.game_settings import GameSettings
 from game.rl_environment.game_tensor_converter import GameTensorConverter
@@ -83,7 +50,12 @@ class GameEnv(EnvBase):
         """Reset environment and return the initial state.
         This method redefines the abstract method _reset() from EnvBase
         """
-        raise NotImplementedError
+
+        # Reset all environments
+        for env in self.environments:
+            env.reset()
+
+        return self.get_state()
 
     def _set_seed(self, seed: int | None):
         """Set seed for the environment."""
@@ -92,31 +64,67 @@ class GameEnv(EnvBase):
     def _step(self, tensordict: TensorDictBase) -> TensorDictBase:
         """Do a step in the environment and return the next state and reward."""
 
-        # TODO: get actions from the input tensordict. Specify format in the class docstring.
+        # Step all environments
+        for i, env in enumerate(self.environments):
+            # TODO: get actions from the input tensordict. Specify format in the class docstring.
+            env.step([])
+
+        step_output = self.get_state()
+
+        # TODO: add the rewards on top of this
+        rewards = torch.zeros(self.batch_size)
+        step_output["reward"] = rewards
+
+        return step_output.to(self.device)
+
+    def get_state(self) -> TensorDictBase:
+        """Get the current state of all environments in a batched tensordict"""
+
+        # Count the max bullets and ennemies for this batch among all environments
+        counts = Environment.get_max_entity_count(self.environments)
+
         player_observations = torch.zeros(self.batch_size + (5,))
+        bullet_observations = torch.zeros(
+            self.batch_size + (counts[EntityType.BULLET], 5)
+        )
+        enemy_observations = torch.zeros(
+            self.batch_size + (counts[EntityType.ENEMY], 5)
+        )
+
+        # NOTE: use this mask tensor in order to avoid learning from environments that have ended.
+        dones = torch.zeros(self.batch_size, dtype=torch.bool)
 
         # Step all environments
         for i, env in enumerate(self.environments):
-            env.step([])
 
             # Convert player observations to tensors
             player_observations[i] = self.converter.player_to_tensor(env.player)
+            dones[i] = env.done
 
-            # TODO: same for ennemies and bullets
+            # Build the entity tensors
+            enemy_count = 0
+            bullet_count = 0
 
-        step_output = TensorDict(
+            for entity in env.entities:
+                if isinstance(entity, EnemyEntity):
+                    enemy_observations[i, enemy_count] = self.converter.enemy_to_tensor(
+                        entity
+                    )
+                    enemy_count += 1
+                elif isinstance(entity, BulletEntity):
+                    bullet_observations[i, bullet_count] = (
+                        self.converter.bullet_to_tensor(entity)
+                    )
+                    bullet_count += 1
+
+        states = TensorDict(
             {
                 "player_obs": player_observations,
-                "enemy_obs": torch.zeros(
-                    1, 1
-                ),  # TODO: replace with actual enemy observations
-                "bullet_obs": torch.zeros(
-                    1, 1
-                ),  # TODO: replace with actual bullet observations
-                "reward": torch.zeros(1, 1),  # TODO: replace with actual reward
-                "done": torch.zeros(1, 1),  # TODO: replace with actual done
+                "enemy_obs": enemy_observations,
+                "bullet_obs": bullet_observations,
+                "done": dones,
             },
             batch_size=self.batch_size,
         )
 
-        return step_output.to(self.device)
+        return states.to(self.device)
